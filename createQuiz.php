67b6fc7308ac2e5c3e3fd13a44491207570bddb5
Ali Nassar <98914243+nassarov@users.noreply.github.com>
2025-04-27 23:39:53 +0300

    Updating createQuiz.php

--- createQuiz.php
+++ createQuiz.php
@@ -1,5 +1,12 @@
 <?php
-require 'conn.php';
+require '../conn.php';
+
 $title = $_POST['title'];
-$sql = "INSERT INTO quizzes (title) VALUES ('$title')";
+$sql = "INSERT INTO quizzes (title) VALUES (?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $title);
+
+$stmt->execute();
+$stmt->close();
+$conn->close();
 ?>
