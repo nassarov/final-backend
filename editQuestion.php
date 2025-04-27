001ccab6f375b9ccce1d039e5bfd9c99c0133d71
Ali Nassar <98914243+nassarov@users.noreply.github.com>
2025-04-27 23:43:44 +0300

    Updating editQuestion.php

--- editQuestion.php
+++ editQuestion.php
@@ -1,8 +1,14 @@
 <?php
-require 'conn.php';
-$id = $_POST['id'];
+require '../conn.php';
+header('Content-Type: application/json');
+$questionId = $_POST['questionId'];
 $value = $_POST['value'];
 
-$sql = "UPDATE questions SET value = '$value' WHERE id = '$id'";
-mysqli_query($conn, $sql)
+$sql = "UPDATE questions SET value = ? WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("si", $value, $questionId);
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
 ?>
